78f0d76763ebeb995994a6a4c4e5cca0bd9ed4ae
Kristoffer Balintona <krisbalintona@gmail.com>
2025-03-23 03:09:29 -0500

    feat: Blacklist for top-level drawers

--- org-hide-drawers.el
+++ org-hide-drawers.el
@@ -26,8 +26,6 @@
 (require 'org)
 (require 'org-element)
 
-;; TODO 2024-11-12: Ensure namespace is adhered to
-
 ;;; Options
 (defgroup org-hide-drawers ()
   "Hide Org drawers using overlays."
@@ -39,7 +37,7 @@
   :type 'string)
 
 (defcustom org-hide-drawers-property-name-blacklist (list)
-  "Regexps that prevent hiding drawers when matching a property name.
+  "Regexps that prevent hiding property drawers when matching a property name.
 A list of regexps that match property names, where matches prevent
 hiding drawer.  If a drawer contains a property that matches any of the
 regexps in this list, that drawer will not be hidden.
@@ -55,8 +53,45 @@ Likewise, if nil, match property names as if `case-fold-search' were
 nil."
   :type 'boolean)
 
-(defcustom org-hide-drawers-hide-top-level t
-  "If nil, don't hide top-level property drawers."
+(defcustom org-hide-drawers-hide-top-level-properties-drawer t
+  "Whether to hide top-level property drawers.
+Top-level property drawers are property drawers (drawers whose name is
+PROPERTY) placed before any heading.
+
+If non-nil, hide top-level property drawers.  Otherwise, don't hide
+top-level property drawers."
+  :type 'boolean)
+
+(defcustom org-hide-drawers-drawer-name-blacklist (list)
+  "Regexps that prevent hiding drawers when matching the drawer's name.
+A list of regexps that match drawer names, where matches prevent hiding
+drawer, top-level (i.e., before any heading in the buffer) or not. If
+the name of a drawer matches any of the regexps in this list, that
+drawer will not be hidden.
+
+For instance, the following drawers will not be matched if \"CONTENTS\"
+is in this list:
+
+  :CONTENTS:
+  This top-level drawer will not be hidden.
+  :END:
+
+  * Heading
+
+  Paragraph text.
+  :CONTENTS:
+  This drawer will also not be hidden.
+  :END:
+
+See also `org-hide-drawers-drawer-name-blacklist-ignore-case-p' for
+whether these regexps are case insensitive or not."
+  :type '(repeat regexp))
+
+(defcustom org-hide-drawers-drawer-name-blacklist-ignore-case-p t
+  "Whether the regexps in `org-hide-drawers-drawer-name-blacklist' ignore case.
+If non-nil, match property names as if `case-fold-search' were non-nil.
+Likewise, if nil, match property names as if `case-fold-search' were
+nil."
   :type 'boolean)
 
 ;;; Variables
@@ -64,9 +99,9 @@ nil."
   "Category of org-hide-drawers overlays.")
 
 ;;; Functions
-(defun org-hide-drawers--get-properties (drawer)
-  "Extract all properties from the given Org DRAWER element."
-  (let ((contents (org-element-contents drawer))
+(defun org-hide-drawers--get-properties (property-drawer)
+  "Extract all properties from the given org drawer (PROPERTY-DRAWER)."
+  (let ((contents (org-element-contents property-drawer))
         properties)
     (dolist (element contents)
       (when (and (eq (org-element-type element) 'node-property)
@@ -76,26 +111,39 @@ nil."
               properties)))
     (nreverse properties)))             ; Return the list in original order
 
+(defun org-hide-drawers--get-drawer-name (drawer-element)
+  "Get name of org drawer (DRAWER-ELEMENT)."
+  (org-element-property :drawer-name drawer-element))
+
 (defun org-hide-drawers--hide-drawer-p (drawer)
   "Predicate for whether DRAWER should be hidden.
-DRAWER is an org-element.
-
-Considers `org-hide-drawers-property-name-blacklist'."
-  (let* ((properties (org-hide-drawers--get-properties drawer))
-         (property-keys (mapcar #'car properties))
-         ;; We check if DRAWER is a top-level drawer by checking if the
-         ;; beginning of the drawer (an org element) is at the first point in
-         ;; the buffer
-         (top-level-p (= 1 (org-element-property :begin drawer))))
-    (and
-     ;; First adhere to the value of `org-hide-drawers-hide-top-level'
-     (or org-hide-drawers-hide-top-level (not top-level-p))
-     ;; Check against properties in the blacklist
-     (not (cl-some (lambda (blacklist-prop-regexp)
-                     (let ((case-fold-search org-hide-drawers-property-name-blacklist-ignore-case-p))
-                       (cl-some (lambda (key) (string-match-p blacklist-prop-regexp key))
-                                property-keys)))
-                   org-hide-drawers-property-name-blacklist)))))
+DRAWER is either a drawer or property-drawer org element.  Considers
+various user options to control which drawers are hidden."
+  (let* ((drawer-type (org-element-type drawer)))
+    (pcase drawer-type
+      ('drawer
+       (let ((case-fold-search org-hide-drawers-drawer-name-blacklist-ignore-case-p))
+         ;; Check drawer name blacklist
+         (not (cl-some (lambda (blacklist-drawer-regexp) (string-match-p blacklist-drawer-regexp drawer-name))
+                       org-hide-drawers-drawer-name-blacklist))))
+      ('property-drawer
+       (let* ((properties (org-hide-drawers--get-properties drawer))
+              (property-keys (mapcar #'car properties))
+              ;; We check if DRAWER is a top-level drawer by checking if the
+              ;; beginning of the drawer (an org element) is at the first point
+              ;; in the buffer
+              (top-level-p (= 1 (org-element-property :begin drawer))))
+         (and
+          ;; Check against top-level property drawers
+          (or org-hide-drawers-hide-top-level-properties-drawer (not top-level-p))
+          ;; Check against property name blacklist
+          (not (cl-some (lambda (blacklist-prop-regexp)
+                          (let ((case-fold-search org-hide-drawers-property-name-blacklist-ignore-case-p))
+                            (cl-some (lambda (key) (string-match-p blacklist-prop-regexp key))
+                                     property-keys)))
+                        org-hide-drawers-property-name-blacklist)))))
+      (t
+       (error "`org-hide-drawers--hide-drawer-p' accepts only drawer and property drawer elements")))))
 
 (defun org-hide-drawers-get-overlays (&optional buffer)
   "Return a list of all org-hide-drawers overlays.
@@ -120,8 +168,8 @@ buffer instead."
     ;; properties
     (overlay-put ov 'category 'org-hide-drawers)
     (overlay-put ov 'display org-hide-drawers-display-string)
-    (overlay-put ov 'modification-hooks '((lambda (overlay _after _beg _end)
-                                            (delete-overlay overlay))))
+    (overlay-put ov 'modification-hooks
+                 (list (lambda (overlay _after _beg _end) (delete-overlay overlay))))
     (overlay-put ov 'read-only t)
     (overlay-put ov 'evaporate t)
     ;; Read (info "(elisp) Invisible Text") for interactions with isearch
@@ -133,8 +181,6 @@ buffer instead."
                    (overlay-put overlay 'display (when hidep org-hide-drawers-display-string))))))
 
 ;;; Commands
-;; TODO 2024-10-23: Consider special behavior for top-level drawers.  See
-;; `org-tidy-should-tidy'.
 (defun org-hide-drawers-make-overlays ()
   "Conditionally hide org drawers in the current buffer using the org AST.
 Hide every drawer in the current buffer if it satisfies
