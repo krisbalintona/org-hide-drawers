18f4e9a18e39f3322ed9c5a93b1e001840bd6cd5
Kristoffer Balintona <krisbalintona@gmail.com>
2025-06-12 01:01:46 -0500

    style: Minor changes to package commentary

--- org-hide-drawers.el
+++ org-hide-drawers.el
@@ -23,16 +23,19 @@
 
 ;;; Commentary:
 
-;; Hide drawers in org-mode buffers using overlays.  These overlays replace the
-;; visual display of drawers using their "display" property.
-
-;; Usage:
-;; 1. Open an org buffer.  (Indirect buffers are supported by org-hide-drawers.)
-;; 2. Enable `org-hide-drawers-mode'.  Drawers become "hidden": their displayed
-;; text is changed to `org-hide-drawers-display-string'.
-;; 3. Un-hide drawers by calling `org-hide-drawers-toggle'.
+;; Hide drawers in org-mode buffers using overlays.  These overlays
+;; replace the visual display of drawers using their "display"
+;; property.
+;; 
+;; Test out the functionality easily:
+;; 1. Open an org buffer.  (Indirect buffers are supported by
+;;    org-hide-drawers.)
+;; 2. Enable `org-hide-drawers-mode'.  Drawers become "hidden": their
+;;    displayed text is changed to `org-hide-drawers-display-string'.
+;; 3. Un-hide drawers by calling `org-hide-drawers-toggle', or
+;;    `org-hide-drawers-make-overlays’.
 ;; 4. Hide drawers by calling `org-hide-drawers-toggle' again, or
-;; `org-hide-drawers-delete-overlays'.
+;;    `org-hide-drawers-delete-overlays'.
 
 ;;; Code:
 (require 'org-element)
