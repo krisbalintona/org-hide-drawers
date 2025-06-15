53bdb6b82a8a5eca6b643d125d1e08f6ae15b67e
Kristoffer Balintona <krisbalintona@gmail.com>
2025-06-15 08:01:40 -0500

    fix: Support for versions of Emacs between 26.1 and 28.1

--- org-hide-drawers.el
+++ org-hide-drawers.el
@@ -277,13 +277,18 @@ If BUFFER is non-nil, delete overlays in that buffer instead."
 ;; specification, because it checks property key names, will never
 ;; match a property drawer that is empty (has no property keys).
 (defcustom org-hide-drawers-display-strings
-  (list (list 'top-level-property-drawer
-              (propertize (concat "[Hidden" (truncate-string-ellipsis) "]") 'face 'shadow))
-        (list 'drawer-regexp
-              (propertize (concat "[Hidden" (truncate-string-ellipsis) "]") 'face 'shadow) (rx (0+ anychar)))
-        (list 'property-drawer-regexp
-              (propertize " #" 'face 'shadow)
-              (rx (0+ anychar))))
+  (let ((ellipsis (cond
+                   ((fboundp 'truncate-string-ellipsis)
+                    (truncate-string-ellipsis))
+                   ((char-displayable-p ?…) "…")
+                   (t "..."))))
+    (list (list 'top-level-property-drawer
+                (propertize (concat "[Hidden" ellipsis "]") 'face 'shadow))
+          (list 'drawer-regexp
+                (propertize (concat "[Hidden" ellipsis "]") 'face 'shadow) (rx (0+ anychar)))
+          (list 'property-drawer-regexp
+                (propertize " #" 'face 'shadow)
+                (rx (0+ anychar)))))
   "Display strings used to hide drawers.
 This is a list of lists.  Each inner list is a specification that for
 the kind of drawer or property drawer that should be matched against, a
